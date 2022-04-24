d06cc24c0e6885b8a086d3f4b296babb9a7a5ec7
charliecici <jianyuan.cao@gmail.com>
2022-04-23 22:29:01 -0400

    update

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,20 @@
 
+
+echo "login AWS console to create the access key/secret key "
+
+
+echo "Initial AWS CLI Environment"
 export AWS_ACCESS_KEY_ID=AKIAUE6WQKX32ML4KU53
 export AWS_SECRET_ACCESS_KEY=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
 export AWS_DEFAULT_REGION=us-east-1
 
+echo "AWS default config"
 aws sts get-caller-identity
 
-aws s3 ls 
+echo "Create S3 bucket"
+aws s3 mb s3://charlie-test-rds-restore-point-in-time --region $AWS_DEFAULT_REGION
 
-#upload CFN
+echo "upload CFN"
 aws s3 cp ./cfn/s3.yml s3://charlie-test-rds-restore-point-in-time/
 
 #CFN Stack Set
